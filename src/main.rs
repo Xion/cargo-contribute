e1d0f0255321d850d70417d0e47a69b3e9ce2311
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2017-12-09 18:02:54 +0100

    More better hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+//!
+//! cargo-contribute
+//!
+
+#[macro_use] extern crate lazy_static;
+
+
+lazy_static! {
+    /// Application / package name, as filled out by Cargo.
+    static ref NAME: &'static str = option_env!("CARGO_PKG_NAME")
+        .unwrap_or("cargo-contribute");
+
+    /// Application version, as filled out by Cargo.
+    static ref VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("{} v{}", *NAME, VERSION.unwrap());
 }
