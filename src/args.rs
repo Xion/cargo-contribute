733328520b57ebd8b858d2ad62d9fbce235b4ba5
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2017-12-18 22:50:54 +0100

    Fix #1 (argument parsing issue)

--- src/args.rs
+++ src/args.rs
@@ -24,8 +24,14 @@ pub fn parse() -> Result<Options, ArgsError> {
 /// (*all* arguments, including binary name).
 #[inline]
 pub fn parse_from_argv<I, T>(argv: I) -> Result<Options, ArgsError>
-    where I: IntoIterator<Item=T>, T: Clone + Into<OsString>
+    where I: IntoIterator<Item=T>, T: Clone + Into<OsString> + PartialEq<str>
 {
+    // Detect `cargo contribute` invocation, and remove the subcommand name.
+    let mut argv: Vec<_> = argv.into_iter().collect();
+    if argv.len() >= 2 && &argv[1] == "contribute" {
+        argv.remove(1);
+    }
+
     let parser = create_parser();
     let matches = try!(parser.get_matches_from_safe(argv));
     Options::try_from(matches)
