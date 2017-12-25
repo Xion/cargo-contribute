f02c41e17bacf2cc0ddd30dfd8d9f08d018519c0
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2017-12-25 16:32:38 +0100

    Reading [package] from Cargo.toml (unused yet)

--- src/issues/cargo_toml.rs
+++ src/issues/cargo_toml.rs
@@ -4,10 +4,27 @@ use std::fs::File;
 use std::io::{self, Read};
 use std::path::Path;
 
-use serde::de::Error as SerdeDeError;
+use serde::de::{Deserialize, Error as SerdeDeError};
 use toml::{self, Value as Toml};
 
-use model::Dependency;
+use model::{Dependency, Package};
+
+
+/// Read [package] information from given Cargo.toml manifest.
+pub fn read_package<P: AsRef<Path>>(manifest_path: P) -> Result<Package, Error> {
+    let path = manifest_path.as_ref();
+    trace!("Reading [package] from manifest: {}", path.display());
+
+    let mut file = File::open(path)?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+
+    let manifest: Toml = toml::from_str(&content)?;
+    let package = manifest.get("package")
+        .ok_or_else(|| Error::Toml(toml::de::Error::custom(format!(
+            "[package] section not found in {}", path.display()))))?;
+    Deserialize::deserialize(package.clone()).map_err(Error::Toml)
+}
 
 
 /// List the dependencies of a crate described by given Cargo.toml manifest.
